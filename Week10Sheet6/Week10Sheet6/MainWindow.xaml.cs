15aacb76bc1e0acac03ee58c058bb040538725d5
Jordan Forde <S00181590@mail.itsligo.ie>
2019-03-27 16:56:57 +0000

    Initial Commit

---
+++ Week10Sheet6/Week10Sheet6/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week10Sheet6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NORTHWNDEntities db = new NORTHWNDEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuery_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c.CompanyName;
+
+            lbxCustomersEx1.ItemsSource = query.ToList<string>();
+        }
+
+        private void btnQueryEx2_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from c in db.Customers
+                        select c;
+
+            dgCUstomersEx2.ItemsSource = query.ToList();
+        }
+
+        private void btnQueryEx3_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from o in db.Orders
+                        where o.Customer.City.Equals("London")
+                        || o.Customer.City.Equals("Paris")
+                        || o.Customer.City.Equals("USA")
+                        orderby o.Customer.CompanyName
+                        select new
+                        {
+                            CustomerName = o.Customer.CompanyName,
+                            City = o.Customer.City,
+                            Address = o.ShipAddress
+                        };
+
+            dgCUstomersEx3.ItemsSource = query.ToList().Distinct();
+        }
+
+        private void btnQueryEx4_Click(object sender, RoutedEventArgs e)
+        {
+            var query = from p in db.Products
+                        where p.Category.CategoryName.Equals("Beverages")
+                        orderby p.ProductID descending
+                        select new
+                        {
+                           p.ProductID,
+                           p.ProductName,
+                           p.Category.CategoryName,
+                           p.UnitPrice
+                        };
+
+            dgCUstomersEx4.ItemsSource = query.ToList().Distinct();
+        }
+
+        private void btnQueryEx5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
